61cf13c3997952686164428a6e1e70cfcc3a9e48
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 07:53:21 +0800

    writing the formula

--- printing_right_triangle_pattern_using_numbers.py
+++ printing_right_triangle_pattern_using_numbers.py
@@ -1,11 +1,15 @@
 
 
 # ----- VISUALIZATION -----
-# 1 
-# 2 2 
-# 3 3 3 
-# 4 4 4 4 
-# 5 5 5 5 5
+# 1             1, 1
+# 2 2           2, 2
+# 3 3 3         3, 3
+# 4 4 4 4       4, 4
+# 5 5 5 5 5     5, 5
+
+# n/height = 5
+# number of index = prev index/variable + 1 
+
 
 # ----- FUNCTIONS -----
 
