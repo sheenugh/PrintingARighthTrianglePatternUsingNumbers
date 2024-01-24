87e32a5b68141a913d592045094556c913fb8d36
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 08:11:02 +0800

    nagana pero need pa iadjust

--- printing_right_triangle_pattern_using_numbers.py
+++ printing_right_triangle_pattern_using_numbers.py
@@ -6,11 +6,17 @@
 # 3 3 3         3*2 - 3, 3 | 0, 1
 # 4 4 4 4       4*2 - 4, 4 | 0, 1
 # 5 5 5 5 5     5*2 - 5, 5 | 0, 1
-# n/height = 5
+
+# n or height = 5
 # number of index = prev index/variable + 1 
 
 
 # ----- FUNCTIONS -----
-
+def right_triangle_using_num(height):
+    for i in range(1, height+1):
+        print((height - i), end= " ")
+        print("1" * (i*2-1))
+        
 # PSEUDO CODES
 # - Actual codes
+right_triangle_using_num(5)
