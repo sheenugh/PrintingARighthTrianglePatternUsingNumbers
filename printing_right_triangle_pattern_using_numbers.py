c976baf8abc291a20c8f67780f60d9eea3f5d90f
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 08:38:38 +0800

    inayos ang visualization para kay sir

--- printing_right_triangle_pattern_using_numbers.py
+++ printing_right_triangle_pattern_using_numbers.py
@@ -1,11 +1,11 @@
 
 
 # ----- VISUALIZATION -----
-# 1                         1*2 - 1, 1 | 0, 2
-# 2 2 2                     2*2 - 1, 2 | 0, 2
-# 3 3 3 3 3                 3*2 - 1, 3 | 0, 2
-# 4 4 4 4 4 4 4             4*2 - 1, 4 | 0, 2
-# 5 5 5 5 5 5 5 5 5         5*2 - 1, 5 | 0, 2
+# 1                         1*2 - 1, 1 | 0, 2 --> #height*2-1 | 0 spaces on the left, 2 spaces on the right
+# 2 2 2                     2*2 - 2, 2 | 0, 2 --> #height*2-2 | 0 spaces on the left, 2 spaces on the right
+# 3 3 3 3 3                 3*2 - 3, 3 | 0, 2 --> #height*2-3 | 0 spaces on the left, 2 spaces on the right
+# 4 4 4 4 4 4 4             4*2 - 4, 4 | 0, 2 --> #height*2-4 | 0 spaces on the left, 2 spaces on the right
+# 5 5 5 5 5 5 5 5 5         5*2 - 5, 5 | 0, 2 --> #height*2-5 | 0 spaces on the left, 2 spaces on the right
 
 # n or height = 5
 # number of index = prev index/variable + 1 
@@ -17,13 +17,12 @@ def right_triangle_using_num(height):
         print((str(i)+ " ") * (i*2-1), end= " ")
         print(" " * (height - i))
 
-right_triangle_using_num(5)
-    
-        # print("2" * (i*2-1), end= " ")
-        # print(" " * (height - i))
-        
-        
-        
-        
 # PSEUDO CODES
 # - Actual codes
+# - Calling the function
+right_triangle_using_num(5)
+
+        
+        
+        
+        
