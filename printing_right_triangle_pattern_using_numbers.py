fd43bc991d3089db03b2ae55c7f3eee5032f6cc6
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 12:19:02 +0800

    inayos ang visualization

--- printing_right_triangle_pattern_using_numbers.py
+++ printing_right_triangle_pattern_using_numbers.py
@@ -1,11 +1,11 @@
 
 
 # ----- VISUALIZATION -----
-# 1                         1*2 - 1, 1 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
-# 2 2                       2*2 - 1, 3 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
-# 3 3 3                     3*2 - 1, 5 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
-# 4 4 4 4                   4*2 - 1, 7 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
-# 5 5 5 5 5                 5*2 - 1, 9 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
+# 1                         1*1 - 0, 1 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
+# 2 2                       2*1 - 0, 2 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# 3 3 3                     3*1 - 0, 3 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
+# 4 4 4 4                   4*1 - 0, 4 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
+# 5 5 5 5 5                 5*1 - 0, 5 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
 
 # n or height = 5
 # number of index = prev index/variable + 1 
