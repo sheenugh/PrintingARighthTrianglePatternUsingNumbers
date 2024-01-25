ef7d4d8dd738eb6d4575516d380b3182107baf28
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 12:12:08 +0800

    inayos ko yong pattern kasi mali

--- printing_right_triangle_pattern_using_numbers.py
+++ printing_right_triangle_pattern_using_numbers.py
@@ -1,11 +1,11 @@
 
 
 # ----- VISUALIZATION -----
-# 1                         1*2 - 1, 1 | 0, 2 --> #height*2-1 | 0 spaces on the left, 2 spaces on the right
-# 2 2 2                     2*2 - 2, 2 | 0, 2 --> #height*2-2 | 0 spaces on the left, 2 spaces on the right
-# 3 3 3 3 3                 3*2 - 3, 3 | 0, 2 --> #height*2-3 | 0 spaces on the left, 2 spaces on the right
-# 4 4 4 4 4 4 4             4*2 - 4, 4 | 0, 2 --> #height*2-4 | 0 spaces on the left, 2 spaces on the right
-# 5 5 5 5 5 5 5 5 5         5*2 - 5, 5 | 0, 2 --> #height*2-5 | 0 spaces on the left, 2 spaces on the right
+# 1                         1*2 - 1, 1 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
+# 2 2                       2*2 - 1, 3 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# 3 3 3                     3*2 - 1, 5 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
+# 4 4 4 4                   4*2 - 1, 7 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
+# 5 5 5 5 5                 5*2 - 1, 9 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
 
 # n or height = 5
 # number of index = prev index/variable + 1 
@@ -13,16 +13,16 @@
 
 # ----- FUNCTIONS -----
 def right_triangle_using_num(height):
-    for i in range(1, height+1):
-        print((str(i)+ " ") * (i*2-1), end= " ")
+    for i in range(0, height):
+        print((str(i)+ " ") * ((i*1)-0), end= " ")
         print(" " * (height - i))
 
 # PSEUDO CODES
 # - Actual codes
 # - Calling the function
-right_triangle_using_num(5)
+right_triangle_using_num(6)
 
-        
+
         
         
         
